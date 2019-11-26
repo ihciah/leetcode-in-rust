ac9a543c5a4aa463524c271f06c74d3c66fab814
ihciah <ihciah@gmail.com>
2019-11-26 16:56:01 +0800

    Add two_sums.

---
+++ src/two_sums.rs
@@ -0,0 +1,28 @@
+pub struct Solution {}
+
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+        for (idx, num) in nums.iter().enumerate() {
+            match map.get(&(target - *num)) {
+                Some(pre_idx) => return vec![*pre_idx as i32, idx as i32],
+                None => {
+                    map.insert(num, idx);
+                }
+            }
+        }
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+    }
+}
