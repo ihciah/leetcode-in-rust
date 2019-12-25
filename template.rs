8b4415ce47177c7dcf05a99312de4726ab8e1aa7
ihciah <ihciah@gmail.com>
2019-12-25 22:59:30 +0800

    Add problem template(Ref: aylei/leetcode-rust).

---
+++ template.rs
@@ -0,0 +1,13 @@
+// __EXTRA_USE__
+pub struct Solution {}
+
+// __PROBLEM_DEFAULT_CODE__
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // __TEST_EXTRA_USE__
+
+    #[test]
+    fn test___PROBLEM_ID__() {}
+}
